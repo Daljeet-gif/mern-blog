8614f5c1ebce15b3092fbc4d303e0b6164af0313
Daljeet-gif <singhdaljeet62837@gmail.com>
2024-08-10 10:28:01 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -18,7 +18,7 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-  // const __dirname = path.resolve();
+  const __dirname = path.resolve();
 
 const app = express();
 app.use(cookieParser());
@@ -32,11 +32,11 @@ app.use("/api/user", userRouter),
   app.use("/api/auth", authRouter),
   app.use("/api/post", postRouter),
   app.use("/api/comment", commentRouter),
-//   app.use(express.static(path.join(__dirname, "/client/dist")));
+  app.use(express.static(path.join(__dirname, "/client/dist")));
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal server Error";
